fc8a589a7b66afa9a0518312aff19530986b0c28
Patrick Liang <pl204511194@yahoo.com>
2021-12-27 08:30:11 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove("main");
+const newHeader = document.createElement("h1");
+newHeader.innerHTML = "Patrick is the champion";
+newHeader.setAttribute("id", "victory");
